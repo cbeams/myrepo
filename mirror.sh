6898b2871963060fbe6a4ba64c8a1d7fb3a3e168
Chris Beams <chris@beams.io>
2016-09-15 00:07:00 +0200

    Tweak

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,6 @@
 git config user.email "travis@travis-ci.org"
 git config user.name "Travis CI"
 
-git remote add mirror https://${GH_TOKEN}@github.com/cbeams/myrepo-mirror
+git remote add mirror https://${GH_TOKEN}@github.com/cbeams/myrepo-mirror > /dev/null 2>&1
 git push -q mirror gh-pages
 git remote remove mirror
